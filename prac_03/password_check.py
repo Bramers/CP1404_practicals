75cec8ce1feba02d4bbf2cefa525b92ad5415e1e
Jordan Bramwell <jordan.bramwell@my.jcu.edu.au>
2021-08-15 15:08:01 +1000

    Refactor password check program to use functions

--- prac_03/password_check.py
+++ prac_03/password_check.py
@@ -1,7 +1,47 @@
+# PASSWORD_MINIMUM_LENGTH = 5
+#
+#
+# function main()
+#     password = get_password()
+#     print_asterisks(password)
+#
+#
+# function get_password()
+#     get password
+#     while length of password < PASSWORD_MINIMUM_LENGTH
+#         display invalid password message
+#         get password
+#     return password
+#
+#
+# function print_asterisks(password)
+#     display "*" for each character in password
+#
+#
+# main()
+
+
 PASSWORD_MINIMUM_LENGTH = 5
 
-password = input("Enter password: ")
-while len(password) < PASSWORD_MINIMUM_LENGTH:
-    print(f"Password must be {PASSWORD_MINIMUM_LENGTH} characters long.")
+
+def main():
+    """Print asterisks based on its length of password"""
+    password = get_password()
+    print_asterisks(password)
+
+
+def get_password():
+    """Get a password of valid length"""
     password = input("Enter password: ")
-print("*" * len(password))
+    while len(password) < PASSWORD_MINIMUM_LENGTH:
+        print(f"Password must be {PASSWORD_MINIMUM_LENGTH} characters long.")
+        password = input("Enter password: ")
+    return password
+
+
+def print_asterisks(password):
+    """Print * for password length"""
+    print("*" * len(password))
+
+
+main()
