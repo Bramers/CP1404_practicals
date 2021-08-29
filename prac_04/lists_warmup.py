802b723fb51692758c1bf49b956d0f85fd8a307b
Jordan Bramwell <jordan.bramwell@my.jcu.edu.au>
2021-08-29 11:19:47 +1000

    Fix comments.

--- prac_04/lists_warmup.py
+++ prac_04/lists_warmup.py
@@ -3,11 +3,11 @@ numbers = [3, 1, 4, 1, 5, 9, 2]
 # numbers[0] : 3
 # numbers[-1] : 2
 # numbers[3] : 1
-# numbers[:-1] : 3, 1, 4, 1, 5, 9 "2"
-# numbers[3:4] : 1 "5"
+# numbers[:-1] : 3, 1, 4, 1, 5, 9
+# numbers[3:4] : 1
 # 5 in numbers : true
 # 7 in numbers : false
-# "3" in numbers : "error" false
+# "3" in numbers : false
 # numbers + [6, 5, 3] : 3, 1, 4, 1, 5, 9, 6, 5, 3
 
 numbers[0] = 10
