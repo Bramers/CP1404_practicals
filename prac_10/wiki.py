3bf1b7e96944bfab4f0afe95b61e7d2b35675574
Jordan Bramwell <jordan.bramwell@my.jcu.edu.au>
2021-10-26 13:52:31 +1000

    Refactor Wiki search to print a page's title, summary and URL

--- prac_10/wiki.py
+++ prac_10/wiki.py
@@ -3,8 +3,8 @@ import wikipedia
 search_phrase = input("search: ")
 while search_phrase != "":
     try:
-        print(wikipedia.summary(search_phrase))
+        wiki_page = wikipedia.page(search_phrase, None, False)
+        print(f"{wiki_page.title} \n{wiki_page.summary} \n{wiki_page.url}")
     except wikipedia.exceptions.DisambiguationError as disambiguation_error:
         print(disambiguation_error.options)
     search_phrase = input("search: ")
-
